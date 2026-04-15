ff65e9f20efcc4a41e00d6417ad2046c14b9b7bc
Sushil Singh <sushilsingh.iit@gmail.com>
2026-04-15 22:30:52 +0530

    temprory code runner

--- 4.patterns/tempCodeRunnerFile.cpp
+++ 4.patterns/tempCodeRunnerFile.cpp
@@ -1,16 +1,13 @@
+#include <iostream>
+using namespace std;
 
-        }
-
-
-        //num2
-
-        for (int j=i; j>0; j--) {
-            cout << j;
+int main() {
+    int n = 10;
+    for (int i=0; i<n; i++) { //n
+        for (int j =0; j<i+1; j++) { //i+1
+            cout << "* "; // put space if you want wnat space 
         }
         cout << endl;
-
     }
     return 0;
-
-
 }
