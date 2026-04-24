e92413e475aabd4fbe533480bba1e9b15862e5a7
Sushil Singh <sushilsingh.iit@gmail.com>
2026-04-25 02:05:50 +0530

    qr genrate using python

---
+++ 0.DSA In Python/1.Qrgernatein.py/qr.py
@@ -0,0 +1,10 @@
+import qrcode
+
+import qrcode
+
+# Create the image
+img = qrcode.make('upi://pay?pa=sushil.4u@ybl&pn=sushil_singh&cu=INR')
+
+# Save it
+img.save("kotak_qr.png")
+print("QR Code generated successfully!")
