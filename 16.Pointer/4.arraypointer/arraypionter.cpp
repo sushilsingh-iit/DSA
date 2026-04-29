4850f67452d18c32c9cb1b91dea9857078eeb5fc
Sushil Singh <sushilsingh.iit@gmail.com>
2026-04-29 23:25:57 +0530

    start array pointer

---
+++ 16.Pointer/4.arraypointer/arraypionter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    cout << arr << endl;   // pointer 
+    cout << *arr << endl;   // pointer  => 1
+
+    return 0;
+
+}
